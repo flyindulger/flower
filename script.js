a78984b2f2eb951709fca2dbf3d8053b2fa22b06
flyindulger <100856157+flyindulger@users.noreply.github.com>
2024-01-31 22:49:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var today = new Date();
-    var newYear = new Date(today.getFullYear(), 0, 1);
-    var daysSinceNewYear = Math.floor((today - newYear) / (1000 * 60 * 60 * 24));
+    var targetDate = new Date(2023, 1, 9); // 注意月份是从0开始计数的，所以1月是0
+    var daysUntilTargetDate = Math.floor((targetDate - today) / (1000 * 60 * 60 * 24));
 
-    document.getElementById('daysSinceNewYear').textContent = daysSinceNewYear;
+    // 如果计算结果是负数，表示当前日期已经超过了目标日期，那么显示0天
+    if (daysUntilTargetDate < 0) {
+        daysUntilTargetDate = 0;
+    }
+
+    document.getElementById('daysSinceNewYear').textContent = daysUntilTargetDate;
 });
